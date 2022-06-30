ae21896c00354c104048515419468be2b6af3215
tyler thibault <tyler.thibault@protonmail.com>
2022-06-30 11:00:20 -0600

    commit

---
+++ 01fundamentals1/Program.cs
@@ -0,0 +1,44 @@
+﻿// for (int i=1; i <=255; i++)
+// {
+//     System.Console.WriteLine(i);
+// }
+
+// for (int i=1; i<=100; i++)
+// {
+//     if (i % 3 == 0 || i % 5 == 0)
+//     {
+//         System.Console.WriteLine(i);
+//     }
+// }
+// for (int i=1; i<=100; i++)
+// {
+//     if (i % 3 == 0 && i % 5 == 0)
+//     {
+//         System.Console.WriteLine("FizzBuzz");
+//     }
+//     else if ( i % 3 == 0)
+//     {
+//         System.Console.WriteLine("Fizz");
+//     }
+//     else if ( i % 5 == 0)
+//     {
+//         System.Console.WriteLine("Buzz");
+//     }
+// }
+int i = 1;
+while (i <= 100)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        System.Console.WriteLine("FizzBuzz");
+    }
+    else if ( i % 3 == 0)
+    {
+        System.Console.WriteLine("Fizz");
+    }
+    else if ( i % 5 == 0)
+    {
+        System.Console.WriteLine("Buzz");
+    }
+    i++;
+}
