cd1699ed879c3c95c58eecf76f4dff6ae3149f98
tyler thibault <tyler.thibault@protonmail.com>
2022-07-01 11:27:33 -0600

    boxing and unboxing

--- 03boxing_unboxing/Program.cs
+++ 03boxing_unboxing/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+List<object> newObjList = new List<object>();
+
+newObjList.Add(7);
+newObjList.Add(28);
+newObjList.Add(-1);
+newObjList.Add(true);
+newObjList.Add("chair");
+
+int sum = 0;
+foreach (var item in newObjList)
+{
+    if (item is int)
+    {
+        sum = sum + (int)item;
+    }
+}
+System.Console.WriteLine(sum);
